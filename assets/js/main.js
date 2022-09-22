8016fca699243e3fe9118e4ad984fcac477d1858
rasouli-git <mahanrasoulii@yahoo.com>
2022-09-22 18:58:41 +0330

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
 const btn = document.querySelector(".btn");
 const messDiv = document.querySelector(".mess-div");
 const closeMess = document.querySelector(".close-code");
-// btn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   messDiv.classList.add("active");
-// });
+const items = document.querySelectorAll(".item");
+const codeInput = document.querySelector('.code-input')
+
 
 closeMess.addEventListener("click", () => {
   messDiv.classList.remove("active");
@@ -32,10 +31,11 @@ const password = document.querySelector("#password");
 const fullName = document.querySelector("#fullName");
 const phone = document.querySelector("#phone");
 const email = document.querySelector("#email");
-
+let index = 0;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkInput();
+  index = 0;
 });
 
 function checkInput() {
@@ -51,29 +51,45 @@ function checkInput() {
     setSuccess(username);
   }
 
-  if(passwordValue === ''){
-    setError(password, "فیلد مورد نظر را تکمیل نمایید")
-  }else{
-    setSuccess(password)
+  if (passwordValue === "") {
+    setError(password, "فیلد مورد نظر را تکمیل نمایید");
+  } else {
+    setSuccess(password);
   }
 
-  if(fullNameValue === ''){
-    setError(fullName, "فیلد مورد نظر را تکمیل نمایید")
-  }else{
-    setSuccess(fullName)
+  if (fullNameValue === "") {
+    setError(fullName, "فیلد مورد نظر را تکمیل نمایید");
+  } else {
+    setSuccess(fullName);
   }
 
-  if(phoneValue === ''){
-    setError(phone, "فیلد مورد نظر را تکمیل نمایید")
-  }else{
-    setSuccess(phone)
+  if (phoneValue === "") {
+    setError(phone, "فیلد مورد نظر را تکمیل نمایید");
+  } else if (!isPhone(phoneValue)) {
+    setError(phone, "شماره را درست وارد نمایید");
+  } else {
+    setSuccess(phone);
   }
 
-  if(emailValue === ''){
-    setError(email, "فیلد مورد نظر را تکمیل نمایید")
-  }else{
-    setSuccess(email)
+  if (emailValue === "") {
+    setError(email, "فیلد مورد نظر را تکمیل نمایید");
+  } else if (!isEmail(emailValue)) {
+    setError(email, "ایمیل وارد شده معتبر نمیباشد");
+  } else {
+    setSuccess(email);
   }
+
+  
+
+  items.forEach((item) => {
+    if (item.classList.contains("success")) {
+      index++;
+      if (index===5) {
+        messDiv.classList.add("active");
+        codeInput.focus();
+      }
+    }
+  });
 }
 
 function setError(input, message) {
@@ -86,4 +102,16 @@ function setError(input, message) {
 function setSuccess(input) {
   const form = input.parentElement;
   form.className = "item success";
+}
+
+const patternPhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{5})$/;
+function isPhone(phone) {
+  return patternPhone.test(phone);
+}
+
+const patternEmail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function isEmail(email) {
+  return patternEmail.test(email);
 }
